efa5c0f592a15201a7dd91b660d7cca12280474d
钱永生 <qianyongsheng@qianyongshengdeMacBook-Air.local>
2017-07-12 14:09:25 +0800

    to 170

--- src/Mpociot/ApiDoc/ApiDocGeneratorServiceProvider.php
+++ src/Mpociot/ApiDoc/ApiDocGeneratorServiceProvider.php
@@ -3,12 +3,11 @@
 namespace Mpociot\ApiDoc;
 
 use Illuminate\Support\ServiceProvider;
-use Mpociot\ApiDoc\Commands\GenerateDocumentation;
 use Mpociot\ApiDoc\Commands\UpdateDocumentation;
+use Mpociot\ApiDoc\Commands\GenerateDocumentation;
 
 class ApiDocGeneratorServiceProvider extends ServiceProvider
 {
-
     /**
      * Bootstrap the application events.
      *
@@ -17,10 +16,18 @@ class ApiDocGeneratorServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->loadViewsFrom(__DIR__.'/../../resources/views/', 'apidoc');
+        $this->loadTranslationsFrom(__DIR__.'/../../resources/lang', 'apidoc');
+
+        $this->publishes([
+            __DIR__.'/../../resources/lang' => $this->resource_path('lang/vendor/apidoc'),
+            __DIR__.'/../../resources/views' => $this->resource_path('views/vendor/apidoc'),
+        ]);
     }
 
     /**
-     * Register the API doc commands
+     * Register the API doc commands.
+     *
+     * @return void
      */
     public function register()
     {
@@ -37,4 +44,15 @@ class ApiDocGeneratorServiceProvider extends ServiceProvider
         ]);
     }
 
+    /**
+     * Return a fully qualified path to a given file.
+     *
+     * @param string $path
+     *
+     * @return string
+     */
+    public function resource_path($path = '')
+    {
+        return app()->basePath().'/resources'.($path ? '/'.$path : $path);
+    }
 }
